e80f35dc75b32571be0c866d7d6714dc02ebaf12
Rubheshan <rubheshanv@gmail.com>
2025-12-11 15:38:27 +0800

    logo centered

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,20 +1,15 @@
 import { Link } from "react-router-dom";
 import "../css/NavBar.css";
-
+import logo from "../assets/images/home_page/logo.png";
 
 function NavBar(){
     return(
         <nav className="navbar">
-            <div className="brand-logo">
-                <Link to="/">Subaru</Link>
-            </div>
-
-            <div>
-                <Link to="/Cars" className="navbar-link">Cars</Link>
-                <Link to="/News" className="navbar-link">News</Link>
-                <Link to="/About" className="navbar-link">About</Link>
-
-            </div>
+                <Link to="/">
+                    <img src={logo}
+                         >
+                    </img>
+                </Link>
         </nav>
     );
 }
