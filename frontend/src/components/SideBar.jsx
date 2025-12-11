59d7bb4ac83e8884f099e231441bcf46c5791027
Rubheshan <rubheshanv@gmail.com>
2025-12-11 16:32:39 +0800

    sidebar content

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -3,10 +3,18 @@ import "../css/SideBar.css";
 function SideBar({isOpen,setIsOpen}){
     return(
         <div className={`sidebar ${isOpen ?  "open" : ""}`}>
-            <button className="close-button" onClick={() => setIsOpen(false)}>Close</button>
-            <p>Sidebar content</p>
-            <p>Sidebar content</p>
-            <p>Sidebar content</p>
+            <div>
+                <button className="close-button" onClick={() => setIsOpen(false)}>Close</button>
+            </div>
+
+            <div className="sidebar-content">
+                <button>Random Bullshit</button>
+                <button>Random Bullshit</button>
+
+                <button>Random Bullshit</button>
+                <button>Random Bullshit</button>
+
+            </div>
 
         </div>
     );
