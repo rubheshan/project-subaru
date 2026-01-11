124fde8e36192dbab3ede43dab04953fe6552634
Rubheshan <rubheshanv@gmail.com>
2026-01-11 11:08:18 +0800

    more locations added

--- backend/src/com/subaru/controller/LocationServlet.java
+++ backend/src/com/subaru/controller/LocationServlet.java
@@ -21,9 +21,21 @@ public class LocationServlet extends HttpServlet {
     private List<Location> getLocationData() {
     List<Location> locations = new ArrayList<>();
 
-    locations.add(new Location("Shah Alam", "Selangor"));
-    locations.add(new Location("Georgetown", "Penang"));
-    locations.add(new Location("Johor Bahru", "Johor"));
+    locations.add(new Location("Petaling Jaya (Motor Image)", "Selangor"));
+    locations.add(new Location("Kota Damansara (Ipoh Auto City)", "Selangor"));
+    locations.add(new Location("Bandar Botanic (Sebangga Auto)", "Selangor"));
+    locations.add(new Location("Glenmarie (Karrus Automotive)", "Selangor"));
+    locations.add(new Location("Kajang (SV Prestige)", "Selangor"));
+    locations.add(new Location("Putrajaya (SS Auto Venture)", "Putrajaya"));
+    locations.add(new Location("Kepong (Robust Synergy)", "Kuala Lumpur"));
+    locations.add(new Location("Bayan Lepas (Gembira Motor)", "Penang"));
+    locations.add(new Location("Butterworth (Gembira Motor)", "Penang"));
+    locations.add(new Location("Ipoh (Ipoh Auto City)", "Perak"));
+    locations.add(new Location("Johor Bahru (Motor Image Malaysia)", "Johor"));
+    locations.add(new Location("Skudai (AK Wheels Marketing)", "Johor"));
+    locations.add(new Location("Seremban (SV Prestige)", "Negeri Sembilan"));
+    locations.add(new Location("Kuantan (Eastern Boxer)", "Pahang"));
+    locations.add(new Location("Kuala Terengganu (Star East Motors)", "Terengganu"));
 
     return locations;
 }
